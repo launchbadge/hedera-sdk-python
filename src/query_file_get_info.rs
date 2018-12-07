1b6319787c78aff2a54d690342bb33d8b160044c
Josh <josh@launchbadge.com>
2018-12-06 16:30:01 -0800

    switched to proxy model for data access

--- src/query_file_get_info.rs
+++ src/query_file_get_info.rs
@@ -6,8 +6,6 @@ use super::timestamp::py_date_time;
 use pyo3::prelude::*;
 use pyo3::types::PyDateTime;
 use crate::errors::PyException;
-use chrono::DateTime;
-use chrono::Utc;
 
 #[pyclass(name = QueryFileGetInfo)]
 pub struct PyQueryFileGetInfo {
@@ -22,43 +20,39 @@ impl PyQueryFileGetInfo {
     }
 }
 
-#[pyclass(name = FileInfo)]
-pub struct FileInfo {
-    #[prop(get)]
-    pub file_id: String,
-    #[prop(get)]
-    pub size: i64,
-    expiration_time: DateTime<Utc>,
-    #[prop(get)]
-    pub deleted: bool,
-    #[prop(get)]
-    pub keys: Vec<String>,
-}
-
 #[pymethods]
 impl PyQueryFileGetInfo {
-    pub fn get(&mut self) -> PyResult<FileInfo> {
+    #[getter]
+    pub fn file_id(&mut self) -> PyResult<String> {
         let response = self.inner.get().map_err(PyException)?;
+        Ok(response.file_id.to_string())
+    }
 
-        Ok(FileInfo {
-            file_id: response.file_id.to_string(),
-            size: response.size,
-            expiration_time: response.expiration_time,
-            deleted: response.deleted,
-            keys: response.keys.into_iter().map(|key| key.to_string()).collect()
-        })
+    #[getter]
+    pub fn size(&mut self) -> PyResult<i64> {
+        let response = self.inner.get().map_err(PyException)?;
+        Ok(response.size)
+    }
+
+    #[getter]
+    pub fn deleted(&mut self) -> PyResult<bool> {
+        let response = self.inner.get().map_err(PyException)?;
+        Ok(response.deleted)
+    }
+
+    #[getter]
+    pub fn keys(&mut self) -> PyResult<String> {
+        let response = self.inner.get().map_err(PyException)?;
+        Ok(response.keys.into_iter().map(|key| key.to_string()).collect())
+    }
+
+    pub fn expiration_time(&mut self, py: Python) -> PyResult<Py<PyDateTime>> {
+        let response = self.inner.get().map_err(PyException)?;
+        py_date_time(response.expiration_time, py)
     }
 
     pub fn cost(&mut self) -> PyResult<u64> {
         self.inner.cost().map_err(PyException)
     }
-}
-
-#[pymethods]
-impl FileInfo {
-    pub fn expiration_time(&mut self, py: Python) -> PyResult<Py<PyDateTime>> {
-        py_date_time(self.expiration_time, py)
-    }
-
 }
 
