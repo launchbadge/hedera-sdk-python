5e231dd9f7fccd5629915570f4a32e67544668c2
Josh <josh@launchbadge.com>
2018-12-06 16:49:54 -0800

    reverted to a hybrid of my two previous versions

--- src/query_file_get_info.rs
+++ src/query_file_get_info.rs
@@ -20,39 +20,50 @@ impl PyQueryFileGetInfo {
     }
 }
 
+#[pyclass(name = FileInfo)]
+pub struct PyFileInfo {
+    inner: QueryFileGetInfoResponse,
+}
+
 #[pymethods]
 impl PyQueryFileGetInfo {
-    #[getter]
-    pub fn file_id(&mut self) -> PyResult<String> {
+    pub fn get(&mut self) -> PyResult<PyFileInfo> {
         let response = self.inner.get().map_err(PyException)?;
-        Ok(response.file_id.to_string())
-    }
 
-    #[getter]
-    pub fn size(&mut self) -> PyResult<i64> {
-        let response = self.inner.get().map_err(PyException)?;
-        Ok(response.size)
-    }
-
-    #[getter]
-    pub fn deleted(&mut self) -> PyResult<bool> {
-        let response = self.inner.get().map_err(PyException)?;
-        Ok(response.deleted)
-    }
-
-    #[getter]
-    pub fn keys(&mut self) -> PyResult<String> {
-        let response = self.inner.get().map_err(PyException)?;
-        Ok(response.keys.into_iter().map(|key| key.to_string()).collect())
-    }
-
-    pub fn expiration_time(&mut self, py: Python) -> PyResult<Py<PyDateTime>> {
-        let response = self.inner.get().map_err(PyException)?;
-        py_date_time(response.expiration_time, py)
+        Ok(PyFileInfo {
+            inner: response
+        })
     }
 
     pub fn cost(&mut self) -> PyResult<u64> {
         self.inner.cost().map_err(PyException)
+    }
+}
+
+#[pymethods]
+impl PyFileInfo {
+    #[getter]
+    pub fn file_id(&mut self) -> PyResult<String> {
+        Ok(self.inner.file_id.to_string())
+    }
+
+    #[getter]
+    pub fn size(&mut self) -> PyResult<i64> {
+        Ok(self.inner.size)
+    }
+
+    #[getter]
+    pub fn deleted(&mut self) -> PyResult<bool> {
+        Ok(self.inner.deleted)
+    }
+
+    #[getter]
+    pub fn keys(&mut self) -> PyResult<String> {
+        Ok(self.inner.keys.iter().map(|key| key.to_string()).collect())
+    }
+
+    pub fn expiration_time(&mut self, py: Python) -> PyResult<Py<PyDateTime>> {
+        py_date_time(self.inner.expiration_time, py)
     }
 }
 
