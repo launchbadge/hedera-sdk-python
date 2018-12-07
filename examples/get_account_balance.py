3d1d069c05a6aa130cca83eaf39116072dadfd1f
Ryan Leckey <leckey.ryan@gmail.com>
2018-12-07 02:23:31 -0800

    Update get_account_balance.py

--- examples/get_account_balance.py
+++ examples/get_account_balance.py
@@ -1,6 +1,8 @@
 import hedera
 
 client = hedera.Client("testnet.hedera.com:50001")
+
+# todo: await client.account("0.0.2").balance
 balance = client.account("0:0:2").balance().get()
 
-print("balance = {} tinybars".format(balance))
+print(f"balance = {balance} tinybars")
