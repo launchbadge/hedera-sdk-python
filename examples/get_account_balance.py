407674c63eb40930c5bfbf9df95dcfe952155b20
Ryan Leckey <ryan@launchbadge.com>
2018-12-07 09:38:16 -0800

    Add hbar conversion to get_account_balance example

--- examples/get_account_balance.py
+++ examples/get_account_balance.py
@@ -6,3 +6,4 @@ client = hedera.Client("testnet.hedera.com:50001")
 balance = client.account("0:0:2").balance().get()
 
 print(f"balance = {balance} tinybars")
+print(f"balance = {balance / 100000000.0} hbars")
