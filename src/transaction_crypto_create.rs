095cd42acea5c4d31723cb30e615e8e6503da6a3
Jason Poindexter <jason@launchbadge.com>
2018-12-12 11:47:16 -0800

    Add remaining parameters to transaction_crypto_create

--- src/transaction_crypto_create.rs
+++ src/transaction_crypto_create.rs
@@ -1,7 +1,14 @@
-use crate::PyPublicKey;
+use crate::{PyPublicKey, PyAccountId};
 use hedera::transaction::TransactionCryptoCreate;
 
 def_transaction!(TransactionCryptoCreate() {
     fn key(&PyPublicKey);
     fn initial_balance(u64);
+    fn proxy_account(&PyAccountId);
+    fn proxy_fraction(i32);
+    fn max_receive_proxy_fraction(i32);
+    // TODO: fn auto_renew_period(Duration);
+    fn send_record_threshold(i64);
+    fn receive_record_threshold(i64);
+    fn receiver_signature_required(bool);
 });
