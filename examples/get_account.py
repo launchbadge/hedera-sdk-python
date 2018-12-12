b37a253f77acf3f73b8839c71c04a6a7bfe97d91
Ryan Leckey <ryan@launchbadge.com>
2018-12-11 16:43:25 -0800

    Clean up get_account example

--- examples/get_account.py
+++ examples/get_account.py
@@ -1,18 +1,13 @@
-import hedera, os
+import hedera
+import os
+
+operator = os.environ["OPERATOR"]
 
 client = hedera.Client("testnet.hedera.com:50001")
 
-print(client.now)
-print("I SHOULD NOT PRINT")
+balance = client.account(operator).balance().get()
 
-# # todo: client.set_operator("0:0:2", lambda: os.environ["OPERATOR_SECRET"])
-# # todo: client.set_operator(hedera.AccountId("0:0:2"), lambda: os.environ["OPERATOR_SECRET"])
-#
-# # todo: await client.account("0.0.2").balance
-# balance = client.account(hedera.AccountId("0:0:2")).balance().get()
-#
-# print(f"balance = {balance} tinybars")
-# print(f"balance = {balance / 100000000.0} hbars")
-#
-# # todo: await client.account("0.0.2").info
-# # todo: info = client.account(hedera.AccountId("0:0:2")).info().get()
+print(f"balance = {balance} tinybars")
+print(f"balance = {balance / 100000000.0} hbars")
+
+# todo: info = client.account(hedera.AccountId("0:0:2")).info().get()
